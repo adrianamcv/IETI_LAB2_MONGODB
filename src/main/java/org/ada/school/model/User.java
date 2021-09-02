190dcd9bac2642e598187742a9021030cde4387f
adrianamcv <42459813+adrianamcv@users.noreply.github.com>
2021-09-02 15:49:34 -0500

    Revert "Codelab implementation"

--- src/main/java/org/ada/school/model/User.java
+++ src/main/java/org/ada/school/model/User.java
@@ -1,35 +1,28 @@
 package org.ada.school.model;
 
 import org.ada.school.dto.UserDto;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.UUID;
 
-@Document
 public class User
 
 {
-    @Id
+
     String id;
 
     String name;
 
-    @Indexed( unique = true )
     String email;
 
     String lastName;
 
     Date createdAt;
 
-    public User()
-    {
-    }
 
     public User( UserDto userDto )
     {
+        id = UUID.randomUUID().toString();
         name = userDto.getName();
         lastName = userDto.getLastName();
         email = userDto.getEmail();
@@ -41,39 +34,8 @@ public class User
         return name;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getId() {
+    public String getId()
+    {
         return id;
     }
 
